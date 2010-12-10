ff62c7e4af4b4e1c2fbeae9862ce21f46d183e87
Martin Rue <martin@invalidcast.com>
2010-12-10 18:16:49 +0000

    Added a basic form post example demonstrating model binding and spark integration.

---
+++ tinyweb.samples.formpost/Handlers/UserRegistrationHandler.cs
@@ -0,0 +1,19 @@
+﻿using tinyweb.framework;
+
+namespace tinyweb.samples.formpost.Handlers
+{
+    public class UserRegistrationHandler
+    {
+        Route route = new Route("users/register");
+
+        public IHandlerResult Get()
+        {
+            return Result.Spark("Views\\Register.spark", "Master.spark");
+        }
+
+        public IHandlerResult Post(UserRegistrationModel model)
+        {
+            return Result.Spark(model, "Views\\Complete.spark", "Master.spark");
+        }
+    }
+}
