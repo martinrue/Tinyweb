6416e7821443f5e2cb9e75b8271925994a9a7c54
Martin Rue <martin@invalidcast.com>
2010-12-21 00:53:11 +0000

    Added new sample project showing the use of the NHaml view engine support.

---
+++ tinyweb.samples.nhaml/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using tinyweb.framework;
+
+namespace tinyweb.samples.nhaml
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Tinyweb.Init();
+        }
+    }
+}
---
+++ tinyweb.samples.nhaml/Handers/HomeHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using tinyweb.framework;
+
+namespace tinyweb.samples.nhaml
+{
+    public class HomeHandler
+    {
+        public IHandlerResult Get()
+        {
+            var model = new List<string> { "item 1", "item 2", "item 3", "item 4", "item 5" };
+
+            return Result.NHaml(model, "Views\\Index.haml");
+        }
+    }
+}
